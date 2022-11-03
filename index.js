af5ca1dbfbcb898f9fc67231a7abc72f6304c2c1
Christian Luna <christianlunaduenas@gmail.com>
2022-11-03 14:21:53 -0700

    Set up, running test

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is your description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage information",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who is contributing",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Run tests",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What is your app licensed under?",
+    choices: ["Boost", "WTFPL", "Mozilla", "Eclipse", "None"],
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// I combined both functions in to one. By using inquirer.
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    fs.writeFileSync("README.md", generateMarkdown(data), (err) => {
+      console.log(err);
+    });
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function for License badges.
+function renderLicenseBadge(license) {
+    if (license === "Boost") {
+        return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    }
+    else if (license === "WTFPL") {
+        return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+    }
+    else if (license === "Mozilla") {
+        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    }
+    else if (license === "Eclipse") {
+        return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+    }
+    else if (license === "None") {
+        return ``
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Contributing](#contributing)
+  5. [Testing Instructions](#tests)
+  6. [License](#license)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## License
+  This app is licensed under ${data.license}
+  ## Questions
+  Come find me at https://github.com/LunaData1111
 
 `;
 }
